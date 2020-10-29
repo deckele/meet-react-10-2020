4cb48c83988831ccdfa6a4446c9b5e4334455555
Deckel, Emanuel (ed038p) <ed038p@intl.att.com>
2020-10-29 11:39:10 +0200

    added calculator initial proj

--- my-counter/src/App.tsx
+++ my-counter/src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Counter } from "./components/counter/counter";
 
 function App() {
+  const [showCounter, setShowCounter] = useState(true);
+  const [counterVisibility, setCounterVisibility] = useState(true);
   return (
     <div className="App">
       <h1>Welcom to my counter app!</h1>
-      <Counter />
+      <button onClick={() => setShowCounter((prev) => !prev)}>
+        toggle show counter
+      </button>
+      <button onClick={() => setCounterVisibility((prev) => !prev)}>
+        toggle counter visibility
+      </button>
+      {showCounter ? (
+        <Counter className={counterVisibility ? "" : "hidden"} />
+      ) : null}
+      <footer>My Footer!</footer>
     </div>
   );
 }
--- my-counter/src/components/counter/counter.tsx
+++ my-counter/src/components/counter/counter.tsx
@@ -3,15 +3,24 @@ import { Button } from "./button/button";
 import { Count } from "./count/count";
 import "./counter.css";
 
-export const Counter = () => {
+type CounterProps = {
+  className: string;
+};
+export const Counter = (props: CounterProps) => {
   const [count, setCount] = useState(0);
   const handleIncrement = () => setCount((prev) => prev + 1);
   const handleDecrement = () => setCount((prev) => prev - 1);
+  const countTimes2 = count * 2;
+  const listItems = [1, 1, 3, 4, 5, 6].map((num, i) => (
+    <li key={i}>item number - {num}</li>
+  ));
   return (
-    <div>
+    <div className={props.className}>
       <Button sign="+" onClick={handleIncrement} />
       <Button sign="-" onClick={handleDecrement} />
       <Count count={count} />
+      <div>Count * 2: {countTimes2}</div>
+      <ul>{listItems}</ul>
     </div>
   );
 };
