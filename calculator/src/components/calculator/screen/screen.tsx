1c4922c41d52f5ec868622406f90ed84a769f71f
Deckel, Emanuel (ed038p) <ed038p@intl.att.com>
2020-11-01 12:10:17 +0200

    added useEffect to calculator

--- calculator/src/components/calculator/screen/screen.tsx
+++ calculator/src/components/calculator/screen/screen.tsx
@@ -5,11 +5,33 @@ type ScreenProps = {
 };
 export function Screen(props: ScreenProps) {
   const [isBlinking, setIsBlinking] = useState(false);
+  const [myArray] = useState({});
   useEffect(() => {
+    // side effect (father) function
     setIsBlinking(true);
-    const intervalId = setInterval(() => setIsBlinking(false), 60);
-    return () => clearInterval(intervalId);
-  }, [props.result]);
+    const id = setTimeout(() => setIsBlinking(false), 100);
+    const count = localStorage.getItem("count") || 0;
+    localStorage.setItem("count", Number(count) + 1 + "");
+    console.log("count is: ", count);
+    // cleanup (return nested) function
+    return () => {
+      console.log("cleanup count is: ", count);
+      clearTimeout(id);
+    };
+  }, [props.result, myArray]);
+
+  // runs after render only if props.result changed
+  useEffect(() => localStorage.setItem("screen", props.result), [props.result]);
+  // onChange: no dependency array as second arg.
+  useEffect(() => {
+    console.log("onChange");
+  });
+  // onMount / onUnmount: empty dependency array.
+  useEffect(() => {
+    console.log("OnMount");
+    return () => console.log("OnUnmount");
+  }, []);
+
   return (
     <div className={"calc-screen " + (isBlinking ? "blink-out" : "")}>
       {props.result}
